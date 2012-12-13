c8ff14ecee05dfecb7b352a035016b84f9819249
Tu Dou <sdhzjc@gmail.com>
2012-12-13 19:26:41 +0800

    add sanityinc-solarized

--- packs/live/colour-pack/config/colour-theme-conf.el
+++ packs/live/colour-pack/config/colour-theme-conf.el
@@ -1,9 +1,9 @@
 (live-add-pack-lib "color-theme")
-(require 'color-theme)
+;(require 'color-theme)
 
 ;; use blackbored colour theme
-(load-file (concat (live-pack-lib-dir) "cyberpunk.el"))
-(load-file (concat (live-pack-lib-dir) "gandalf.el"))
-
-(color-theme-cyberpunk)
-(set-cursor-color "yellow")
+;(load-file (concat (live-pack-lib-dir) "cyberpunk.el"))
+;(load-file (concat (live-pack-lib-dir) "gandalf.el"))
+(load-file (concat (live-pack-lib-dir) "sanityinc-solarized-dark-theme.el"))
+(color-theme-sanityinc-solarized-dark)
+;(set-cursor-color "yellow")
